409e4bdbbd55b1506502da8cedf4f7363b286660
Виктория Захарова <zaharova181114@gmail.com>
2023-12-08 23:47:59 +0200

    all section

--- js/common.js
+++ js/common.js
@@ -7,3 +7,22 @@ $('.btn-burger').on('click', function () {
 $('.mobile-menu__close').on('click', function () {
 	$('.mobile-menu').fadeOut();
 });
+
+$('.our-fleet-slider').slick({
+	slidesToShow: 1,
+	variableWidth: true,
+	centerMode: true,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	appendArrows: '.our-fleet-slider__nav',
+	dots: true
+});
+
+$('.reviews-slider').slick({
+	slidesToShow: 1,
+	variableWidth: true,
+	centerMode: false,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	appendArrows: '.reviews-slider__nav',
+});
