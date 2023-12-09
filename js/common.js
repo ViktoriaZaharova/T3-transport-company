5e2861b0e050b6dcb8a3362b3d448d5625f89047
Виктория Захарова <zaharova181114@gmail.com>
2023-12-09 22:22:45 +0200

    all

--- js/common.js
+++ js/common.js
@@ -15,7 +15,17 @@ $('.our-fleet-slider').slick({
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
 	appendArrows: '.our-fleet-slider__nav',
-	dots: true
+	dots: true,
+	responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				variableWidth: false,
+				centerMode: false,
+			}
+		}
+	]
 });
 
 $('.reviews-slider').slick({
@@ -25,4 +35,34 @@ $('.reviews-slider').slick({
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
 	appendArrows: '.reviews-slider__nav',
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				variableWidth: false,
+				centerMode: false,
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				variableWidth: false,
+				centerMode: false,
+			}
+		}
+	]
+});
+
+$('.go_to').click(function (e) {
+	e.preventDefault();
+	var scroll_el = $(this).attr('href');
+	if ($(scroll_el).length !== 0) {
+		$('html, body').animate({
+			scrollTop: $(scroll_el).offset().top
+		}, 500);
+	}
+	return false;
+	$('.mobile-menu').fadeOut();
 });
