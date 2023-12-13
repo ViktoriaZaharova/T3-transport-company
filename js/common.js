e91087b618c2fcb631e8efa5b6496dba15db2729
Виктория Захарова <zaharova181114@gmail.com>
2023-12-13 12:02:15 +0200

    edit parallax

--- js/common.js
+++ js/common.js
@@ -111,11 +111,4 @@ $(window).scroll(function () {
 new WOW().init();
 
 // parallax
-var scene1 = document.getElementById('scene1');
-var parallaxInstance1 = new Parallax(scene1);
-
-var scene2 = document.getElementById('scene2');
-var parallaxInstance2 = new Parallax(scene2);
-
-var scene3 = document.getElementById('scene3');
-var parallaxInstance3 = new Parallax(scene3);
+var rellax = new Rellax('.rellax');
